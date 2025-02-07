5150f7a5d9f2c577791951f205ca5e90b9f938c8
LC1D_ヨシダ_ハルト <146914696+yo-shi-da-h@users.noreply.github.com>
2025-02-07 19:43:35 +0900

    a

--- KamataEngine/PG3_05_01/main.cpp
+++ KamataEngine/PG3_05_01/main.cpp
@@ -1,12 +1,16 @@
 #include <Novice.h>
+#include "Scene.h"
 
-const char kWindowTitle[] = "学籍番号";
+const char kWindowTitle[] = "LE2*_**_***_***";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	Scene* scene = new Scene();
+	scene->Initialize();
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -21,21 +25,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		memcpy(preKeys, keys, 256);
 		Novice::GetHitKeyStateAll(keys);
 
-		///
 		/// ↓更新処理ここから
-		///
-
-		///
+		scene->Update();
 		/// ↑更新処理ここまで
-		///
 
-		///
 		/// ↓描画処理ここから
-		///
-
-		///
+		scene->Draw();
 		/// ↑描画処理ここまで
-		///
 
 		// フレームの終了
 		Novice::EndFrame();
